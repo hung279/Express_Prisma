323e09a7f304dc0d933620564fc3c359ee11616d
hung279 <hungnher279@gmail.com>
2023-05-10 21:31:58 +0700

    add config app

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,9 @@
-import express from "express";
+import App from "./app";
 import dotenv from "dotenv";
+import UserController from "./controllers/user.controller";
 
 dotenv.config();
-const PORT = process.env.PORT || 8080;
-const app = express();
+const PORT = process.env.PORT || 8000;
+const app = new App([new UserController()], PORT);
 
-app.get("/", (req, res) => {
-  res.send("Hello");
-})
-
-app.listen(PORT, () => {
-  console.log(`Running on http://localhost:${PORT}`);
-})
+app.listen();
